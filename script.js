a4fafea573b0e4c16a36ad25f81492ff735839e4
katedominjeviar07 <32132248+katedominjeviar07@users.noreply.github.com>
2019-06-15 06:41:19 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$("#pic1").click(function(){
+    $("#luna").text("LUNA");
+});
+
+$("#pic2").click(function(){
+    $("#pic.3").fadeToggle();
+});
+
+$("#pic3").click(function(){
+    $("#pic.3").slideToggle();
+}); 
+
+$("#pic4").click(function(){
+    $("pic.4").toggle();
+});
